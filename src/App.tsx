a6c9e92744817897c090158e9c2b4909aaac6de8
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-10 01:28:50 +0200

    refactor: implement search route & component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Nav from './components/Nav'
+import Search from './components/pages/Search'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
       </Routes>
     </Router>
   )
