208782640326901b3902c487ec5e9fa8a568c8ad
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-13 00:07:31 +0200

    todo: save token in a cookie and redirect

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,16 +1,47 @@
 import Modal from '../ui/Modal'
 import '../../styles/login.css'
+import { FormEvent, useState } from 'react'
+import { toast } from 'sonner'
 
 export default function Login() {
+  const [email, setEmail] = useState<string>()
+  const [password, setPassword] = useState<string>()
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    try {
+      const result = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/users/login`,
+        {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      ).then((res) => res.json())
+
+      if (!result.success) {
+        toast.error(result.message)
+      }
+    } catch (e) {
+      toast.error(e as string)
+    }
+  }
+
   return (
     <Modal>
       <h1>Login</h1>
       <span className='subtitle'>
         Welcome back to your favorite social media!
       </span>
-      <form className='login-form'>
-        <input type='email' placeholder='Email' />
-        <input type='password' />
+      <form className='login-form' onSubmit={handleSubmit}>
+        <input
+          type='email'
+          placeholder='Email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input type='password' onChange={(e) => setPassword(e.target.value)} />
 
         <input type='submit' value='Send' />
       </form>
