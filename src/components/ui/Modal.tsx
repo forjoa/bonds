4c19301232bbe4ec2f5f6a3cb97b476419179fee
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-12 23:03:27 +0200

    fix: add layout for modal

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -5,5 +5,9 @@ interface ModalProps {
   children: ReactNode
 }
 export default function Modal({ children }: ModalProps) {
-  return <main className='modal'>{children}</main>
+  return (
+    <main className='modal-layout'>
+      <section className='modal'>{children}</section>
+    </main>
+  )
 }
