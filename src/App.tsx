e87a8539ed1fa76548dc0ce109c3544085abcc5e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-12 23:19:01 +0200

    build: add login route

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,22 @@ import Search from './components/pages/Search'
 import Messages from './components/pages/Messages'
 import Notifications from './components/pages/Notifications'
 import Upload from './components/pages/Upload'
+import Login from './components/pages/Login'
 
 function App() {
   return (
     <Router>
-      <Nav />
+      <Nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/messages' element={<Messages />} />
+          <Route path='/notifications' element={<Notifications />} />
+          <Route path='/upload' element={<Upload />} />
+        </Routes>
+      </Nav>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/messages' element={<Messages />} />
-        <Route path='/notifications' element={<Notifications />} />
-        <Route path='/upload' element={<Upload />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   )
