1cdfd42479fa10d3a9d4f87c130b0c1903e1620d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-14 13:53:46 +0200

    feat: types file

---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+import { Dispatch, SetStateAction } from 'react'
+
+export interface UserI {
+  bio: string
+  createdat: Date
+  email: string
+  fullname: string
+  phone: string
+  profilephoto: string | null
+  updatedat: Date
+  userid: number
+  username: string
+}
+
+export interface UserContextType {
+  user: UserI | object
+  setUser: Dispatch<SetStateAction<UserI>>
+}
