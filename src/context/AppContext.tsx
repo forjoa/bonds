9c430ad944dd6f9033e397b82f75e5c0c771ec9d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-14 14:01:40 +0200

    build: create  app context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,12 +1,41 @@
-import { createContext, PropsWithChildren, useContext, useState } from 'react'
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { UserContextType } from '../types/types'
 
-const UserContext = createContext({})
+const UserContext = createContext<UserContextType>({
+  tokenIsValid: false,
+  user: {},
+  setUser: () => {},
+})
 
 export const UserProvider = ({ children }: PropsWithChildren) => {
-  const [user, setUser] = useState(null) // Aquí manejarás los datos del usuario
+  const [user, setUser] = useState({})
+  const [tokenIsValid, setTokenIsValid] = useState(false)
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/auth/verifyToken`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: localStorage.getItem('userbonds'),
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        setTokenIsValid(response.success)
+        setUser(response.user)
+      })
+  }, [])
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ tokenIsValid, user, setUser }}>
       {children}
     </UserContext.Provider>
   )
