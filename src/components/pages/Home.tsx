281c4224bf17df9dfb8500494233cd6bf014a889
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-18 16:15:21 +0200

    todo: fix post card ui

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
 import Container from '../ui/Container'
+import { getHome } from '../../lib/getHome'
+import { useUser } from '../../context/AppContext'
+import { PostsHomeI } from '../../types/types'
 
 export default function Home() {
+  const [posts, setPosts] = useState<PostsHomeI[]>([])
+  const { user } = useUser()
+
+  useEffect(() => {
+    if ('userid' in user) {
+      getHome(user.userid as number).then((res) => {
+        setPosts(res)
+      })
+    }
+  }, [user])
+
   return (
     <Container>
       <h1>Home</h1>
+      {posts.map((post, index) => (
+        <div key={index}>
+          <p>{post.content}</p>
+        </div>
+      ))}
     </Container>
   )
 }
