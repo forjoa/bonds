fda69e7fa60c66caf508544e7fe38e81bad23e0a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-19 23:45:50 +0200

    feat: implement post card component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,16 +3,14 @@ import Container from '../ui/Container'
 import { getHome } from '../../lib/getHome'
 import { useUser } from '../../context/AppContext'
 import { PostsHomeI } from '../../types/types'
-import { useNavigate } from 'react-router'
 import '../../styles/home.css'
-import Flicking from '@egjs/react-flicking'
 import '@egjs/react-flicking/dist/flicking.css'
+import PostCard from '../ui/PostCard'
 import { ViewportSlot } from '@egjs/react-flicking'
 
 export default function Home() {
   const [posts, setPosts] = useState<PostsHomeI[]>([])
   const { user } = useUser()
-  const navigate = useNavigate()
 
   useEffect(() => {
     if ('userid' in user) {
@@ -27,43 +25,11 @@ export default function Home() {
       <h1>Home</h1>
       <aside className='posts-container'>
         {posts.map((post, index) => (
-          <section key={index}>
-            <header>
-              {post.profilephoto && (
-                <img src={post.profilephoto} alt='User photo' />
-              )}
-              <nav onClick={() => navigate(`/users/${post.userid}`)}>
-                <p>{post.fullname}</p>
-                <span className='subtitle'>@{post.username}</span>
-              </nav>
-            </header>
-            <main>
-              <p>{post.content}</p>
-              <Flicking
-                align='prev'
-                circular={false}
-                bounce={200}
-                bound={true}
-                duration={500}
-                moveType='snap'
-                preventDefaultOnDrag={true}
-                className='flicking-wrapper'
-              >
-                {post.photos.map((url, index) => (
-                  <img
-                    src={url}
-                    alt='Post photo'
-                    key={index}
-                    className='flicking-panel'
-                  />
-                ))}
-                <ViewportSlot>
-                  <div className='flicking-panel last-panel' />
-                </ViewportSlot>
-              </Flicking>
-            </main>
-          </section>
+          <PostCard post={post} key={index} />
         ))}
+        <ViewportSlot>
+          <div className='flicking-panel last-panel' />
+        </ViewportSlot>
       </aside>
     </Container>
   )
