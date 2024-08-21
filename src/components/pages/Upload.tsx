db3221722b12c7b0928a1f6eda3c40f93f184e9e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-21 22:32:39 +0200

    feat: upload a post with photos

--- src/components/pages/Upload.tsx
+++ src/components/pages/Upload.tsx
@@ -1,9 +1,85 @@
+import UploadFiles from '../cloudinary/PostPhotos'
 import Container from '../ui/Container'
+import '../../styles/uploadpost.css'
+import { FormEvent, useState } from 'react'
+import { IconX } from '@tabler/icons-react'
+import { useUser } from '../../context/AppContext'
+import { toast } from 'sonner'
 
 export default function Upload() {
+  const [files, setFiles] = useState<string[]>([])
+  const [loading, setLoading] = useState<boolean | null>(null)
+  const [content, setContent] = useState<string>('')
+  const { user } = useUser()
+
+  const removeFile = (index: number) => {
+    setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index))
+  }
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if ('userid' in user && (content !== '' || files.length !== 0)) {
+      const result = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/posts/upload`,
+        {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ userid: user.userid, content, files }),
+        }
+      ).then((res) => res.json())
+
+      if (result.success) {
+        toast.success(result.message)
+      } else {
+        toast.error(result.message)
+      }
+    } else {
+      toast.error('Complete the content in order to upload the post.')
+    }
+  }
+
   return (
     <Container>
       <h1>Upload</h1>
+      <form className='upload-post' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Your content here'
+          className='content'
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <UploadFiles
+          files={files}
+          setFiles={setFiles}
+          loading={loading}
+          setLoading={setLoading}
+        />
+        {loading ? (
+          <span className='subtitle'>Uploading...</span>
+        ) : (
+          <div className='preview-container'>
+            {files.map((file, index) => (
+              <div key={index} className='preview-item'>
+                {file.includes('image') ? (
+                  <img src={file} alt={`Preview ${index + 1}`} />
+                ) : (
+                  <video src={file} controls />
+                )}
+                <button
+                  className='remove-btn'
+                  onClick={() => removeFile(index)}
+                >
+                  <IconX size={18} />
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+        <input type='submit' value='Upload post' className='submit' />
+      </form>
     </Container>
   )
 }
