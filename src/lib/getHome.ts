b5e4a5755b6b811683e490706eb3e611e65b7a3d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-23 22:17:51 +0200

    refactor: receive and send new params

--- src/lib/getHome.ts
+++ src/lib/getHome.ts
@@ -1,4 +1,4 @@
-export const getHome = async (userid: number) => {
+export const getHome = async (userid: number, page: number, limit: number) => {
   const result = await fetch(
     `${import.meta.env.VITE_API_URL}/api/posts/getHome`,
     {
@@ -6,7 +6,7 @@ export const getHome = async (userid: number) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userid }),
+      body: JSON.stringify({ userid, page, limit }),
     }
   ).then((res) => res.json())
 
