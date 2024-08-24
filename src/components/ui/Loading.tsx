f0cedafb2c0f42923780c5b43e97e7e0dc284cac
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-24 22:27:02 +0200

    build: implement loader to loading component

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,9 +1,19 @@
 import '../../styles/loading.css'
+import { Comment } from 'react-loader-spinner'
 
-export default function Loading () {
-    return (
-        <div className="loading">
-            Loading...
-        </div>
-    )
+export default function Loading() {
+  return (
+    <div className='loading'>
+      <Comment
+        visible={true}
+        height='150'
+        width='150'
+        ariaLabel='comment-loading'
+        wrapperStyle={{}}
+        wrapperClass='comment-wrapper'
+        color='#000'
+        backgroundColor='#999'
+      />
+    </div>
+  )
 }
