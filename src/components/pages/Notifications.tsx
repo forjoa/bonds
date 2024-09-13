5045fd6bcfa2baaebe1e2766e03e3c9c4c876971
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-09-13 22:28:59 +0200

    feat: mark all the notifications as read

--- src/components/pages/Notifications.tsx
+++ src/components/pages/Notifications.tsx
@@ -4,6 +4,7 @@ import { useUser } from '../../context/AppContext'
 import { NotificationInfoI } from '../../types/types'
 import NotificationCard from '../ui/NotificationCard'
 import '../../styles/notifications.css'
+import { IconBook } from '@tabler/icons-react'
 
 export default function Notifications() {
   const [notifications, setNotifications] = useState<NotificationInfoI[]>([])
@@ -11,31 +12,75 @@ export default function Notifications() {
 
   useEffect(() => {
     if ('userid' in user) {
-      fetch(
-        `${import.meta.env.VITE_API_URL}/api/notifications/getNotifications`,
-        {
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ id: user.userid }),
-        }
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          setNotifications(data.reverse())
-        })
+      fetchNotifications()
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user])
+
+  const fetchNotifications = async () => {
+    if ('userid' in user) {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/notifications/getNotifications`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id: user.userid }),
+          }
+        )
+        const data = await response.json()
+        setNotifications(data.reverse())
+      } catch (error) {
+        console.error('Error fetching notifications:', error)
+      }
+    }
+  }
+
+  const markAllAsRead = async () => {
+    if ('userid' in user) {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/notifications/markAsReadAll`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id: user.userid }),
+          }
+        )
+
+        if (response.ok) {
+          setNotifications((prevNotifications) =>
+            prevNotifications.map((notification) => ({
+              ...notification,
+              seen: true,
+            }))
+          )
+        } else {
+          console.error('Failed to mark notifications as read')
+        }
+      } catch (error) {
+        console.error('Error marking notifications as read:', error)
+      }
+    }
+  }
 
   return (
     <Container>
       <h1>Notifications</h1>
       <section className='notifications-container'>
-        {notifications &&
-          notifications.map((notification, index) => (
-            <NotificationCard notification={notification} key={index} />
-          ))}
+        <div>
+          <button onClick={markAllAsRead}>
+            <IconBook stroke={1.5} />
+            Mark all as read
+          </button>
+        </div>
+        {notifications.map((notification, index) => (
+          <NotificationCard key={index} notification={notification} />
+        ))}
       </section>
     </Container>
   )
