0a3da470a34e80f593ee194cf12394ef6b72c754
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-09-21 21:28:30 +0200

    refactor: update logic to protect routes

--- src/components/auth/ProtectedRoutes.tsx
+++ src/components/auth/ProtectedRoutes.tsx
@@ -1,16 +1,23 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useUser } from '../../context/AppContext'
 import Loading from '../ui/Loading'
+import { useEffect, useState } from 'react'
 
 export default function ProtectedRoutes() {
-  const localUser = localStorage.getItem('userbonds')
   const { tokenIsValid, loading } = useUser()
+  const [shouldRedirect, setShouldRedirect] = useState(false)
 
-  if (loading) {
-    return <Loading />
+  useEffect(() => {
+    if (!loading && !tokenIsValid) {
+      setShouldRedirect(true)
+    }
+  }, [loading, tokenIsValid])
+
+  if (loading) return <Loading />
+
+  if (shouldRedirect) {
+    return <Navigate to='/login' />
   }
-
-  if (!localUser || !tokenIsValid) return <Navigate to='/login' />
 
   return <Outlet />
 }
